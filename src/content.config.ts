fe7427e14937b0662ac5d4c0bcc44505b398c555
Joshua Tighe <jtighe3@nd.edu>
2026-02-05 10:41:19 +0000

    blog collection extra

---
+++ src/content.config.ts
@@ -0,0 +1,22 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.md', base: './src/blog' }),
+	schema: z.object({
+		title: z.string(),
+		pubDate: z.date(),
+		description: z.string(),
+		author: z.string(),
+		image: z.object({
+			url: z.string(),
+			alt: z.string(),
+		}),
+		tags: z.array(z.string()),
+	}),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
